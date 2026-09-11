bf80c1f3c2cac28661c1d5a73e984bdadeb7fd9b
Ariee <sonnenwaldju@gmail.com>
2026-09-11 14:42:07 +0200

    Add personal raid checklists, movable suitcase shortcut and Sync 0.3.13

--- lichtloot-api/src/addon-beta-manifest.js
+++ lichtloot-api/src/addon-beta-manifest.js
@@ -1,23 +1,23 @@
 export const betaArtifacts = {
   "windows": {
-    "name": "GuildLoot-Sync-0.3.12-Windows-Setup.exe",
+    "name": "GuildLoot-Sync-0.3.13-Windows-Setup.exe",
     "type": "application/octet-stream",
-    "size": 115649911,
-    "sha256": "bfdeb664389927806cbcdcf9b50609f918bc5968158c178955e27b83c6610d8f",
-    "url": "https://github.com/Ariee-Lichtbringer/lichtloot/releases/download/guildloot-sync-beta-0.3.12/GuildLoot-Sync-0.3.12-Windows-Setup.exe.enc?sha256=bfdeb664389927806cbcdcf9b50609f918bc5968158c178955e27b83c6610d8f"
+    "size": 115654349,
+    "sha256": "858655ff4804922e8bc07cc652fa0e48feabf63e80edd9c131da92070c0d0415",
+    "url": "https://github.com/Ariee-Lichtbringer/lichtloot/releases/download/guildloot-sync-beta-0.3.13/GuildLoot-Sync-0.3.13-Windows-Setup.exe.enc?sha256=858655ff4804922e8bc07cc652fa0e48feabf63e80edd9c131da92070c0d0415"
   },
   "mac-arm64": {
-    "name": "GuildLoot-Sync-0.3.12-mac-Apple-Silicon.pkg",
+    "name": "GuildLoot-Sync-0.3.13-mac-Apple-Silicon.pkg",
     "type": "application/octet-stream",
-    "size": 130007670,
-    "sha256": "c5e72685e1778c430952ce36d428dd147da82cf016935eb8b742c77283fb0097",
-    "url": "https://github.com/Ariee-Lichtbringer/lichtloot/releases/download/guildloot-sync-beta-0.3.12/GuildLoot-Sync-0.3.12-mac-Apple-Silicon.pkg.enc?sha256=c5e72685e1778c430952ce36d428dd147da82cf016935eb8b742c77283fb0097"
+    "size": 130022518,
+    "sha256": "d8294fb7f44ac4bcf20264704bc71220c193ace54b6517f9c6494037a39a85a2",
+    "url": "https://github.com/Ariee-Lichtbringer/lichtloot/releases/download/guildloot-sync-beta-0.3.13/GuildLoot-Sync-0.3.13-mac-Apple-Silicon.pkg.enc?sha256=d8294fb7f44ac4bcf20264704bc71220c193ace54b6517f9c6494037a39a85a2"
   },
   "mac-x64": {
-    "name": "GuildLoot-Sync-0.3.12-mac-Intel.pkg",
+    "name": "GuildLoot-Sync-0.3.13-mac-Intel.pkg",
     "type": "application/octet-stream",
-    "size": 135563726,
-    "sha256": "46281a8e08bd9284fa3c95d9b428f7d9c5760261568d581ca272f9f1c59e8ee0",
-    "url": "https://github.com/Ariee-Lichtbringer/lichtloot/releases/download/guildloot-sync-beta-0.3.12/GuildLoot-Sync-0.3.12-mac-Intel.pkg.enc?sha256=46281a8e08bd9284fa3c95d9b428f7d9c5760261568d581ca272f9f1c59e8ee0"
+    "size": 135561252,
+    "sha256": "d18d2920d5c4a9d3e7ae4445ae2f8a38b091b231ba5568a33148f2da1a199c30",
+    "url": "https://github.com/Ariee-Lichtbringer/lichtloot/releases/download/guildloot-sync-beta-0.3.13/GuildLoot-Sync-0.3.13-mac-Intel.pkg.enc?sha256=d18d2920d5c4a9d3e7ae4445ae2f8a38b091b231ba5568a33148f2da1a199c30"
   }
 };
